390227e94d41b6588630139554b0dcea44a137ae
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-03-28 18:28:02 +0100

    Added content

---
+++ data.js
@@ -0,0 +1,173 @@
+data = [
+  {
+    background: 'wilmot.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/839870/header.jpg?t=1708399934',
+    description:
+      "Bei <b>Wilmot's Warehouse</b> spielt man in einem Warenhaus. Man bekommt jeden Tag Gegenstände geliefert und muss diese ordnen, damit man sie wiederfindet, wenn man sie Käufern ausgeben muss. Und das unter Zeitdruck.",
+    story: true,
+    players: 2,
+    minplayers: 1,
+    published: '28.8.2019',
+    lastPlayed: '12.11.2023',
+    multiplayer: 'Lokal',
+    rating: 5,
+    time: '30h 49min',
+  },
+  {
+    background: 'fnaf6.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/738060/header.jpg?t=1709080873',
+    description:
+      "In <b>Freddy Fazbear's Pizzeria Simulator</b> verwaltet man tagsüber seine Pizzeria und kümmert sich nachts um die Animatronics, die man besitzt...",
+    minplayers: 1,
+    published: '4.12.2017',
+    lastPlayed: '23.3.2021',
+    rating: 4,
+    time: '49h 33min',
+  },
+  {
+    background: 'generic/default.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/32340/header.jpg?t=1711473830',
+    description:
+      'In <b>Loom</b> lernt man Zaubersprüche in Form von Notenabfolgen. Diese muss man sich einprägen, damit man sie später aus dem Gedächtnis abspielen kann, um sie anzuwenden.',
+    story: true,
+    minplayers: 1,
+    published: '31.12.1989',
+    lastPlayed: '19.8.2022',
+    rating: 4,
+    time: '2h 14min',
+  },
+  {
+    background: 'generic/emerald.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/289070/header.jpg?t=1710148141',
+    description:
+      "Bei <b>Sid Meier's Civilization VI</b> baut man eine Zivilisation auf, erobert die Welt, kämpft gegen andere und schließt Abkommen mit anderen Zivilisationen.",
+    players: 12,
+    minplayers: 1,
+    published: '20.10.2016',
+    multiplayer: 'Lokal, Online, LAN',
+    rating: 5,
+    time: '33h 1min',
+  },
+  {
+    background: 'absolute_drift.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/320140/header.jpg?t=1709550185',
+    description:
+      "In <b>Absolute Drift</b> fährt man durch die Welt und muss sich verschiedensten Aufgaben stellen, die durch's Driften gelöst werden können. ",
+    story: true,
+    minplayers: 1,
+    published: '29.7.2015',
+    lastPlayed: '25.8.2022',
+    rating: 3,
+    time: '3h 45min',
+    achievements: {
+      progress: 5,
+      total: 20,
+    },
+  },
+  {
+    background: 'summerland.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/1444110/header.jpg?t=1709080435',
+    description:
+      'Bei <b>Summerland</b> geht es um einen Polizisten, der nach seinem Tod nochmal auf seine Taten zurückblickt. Dabei wird klar, dass er in seinem Beruf moralisch fragwürdige Entscheidungen getroffen hat...',
+    story: true,
+    minplayers: 1,
+    published: '1.12.2020',
+    lastPlayed: '3.4.2021',
+    rating: 5,
+    time: '2h 30min',
+    achievements: {
+      progress: 9,
+      total: 9,
+    },
+  },
+  {
+    background: 'human_fall_flat.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/477160/header.jpg?t=1710436632',
+    description:
+      'In <b>Human: Fall Flat</b> läuft man durch eine Welt und löst Rätsel mit seinen zwei Händen, die man direkt ansteuern kann. Durch das Schieben, Ziehen und Drücken von Sachen kann man alle Rätsel lösen.',
+    story: true,
+    players: 8,
+    minplayers: 1,
+    published: '22.7.2016',
+    lastPlayed: '26.11.2023',
+    multiplayer: 'Lokal, Online',
+    rating: 5,
+    time: '55h 37min',
+    achievements: {
+      progress: 49,
+      total: 115,
+    },
+  },
+  {
+    background: 'carcassonne.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/598810/header.jpg?t=1711107003',
+    description:
+      '<b>Carcassonne</b> das Brettspiel am PC. Man muss versuchen die gegebenen Teile möglichst so zu platzieren, dass man möglichst viel Land in Besitz nimmt.',
+    players: 6,
+    minplayers: 1,
+    published: '29.11.2017',
+    lastPlayed: '18.9.2021',
+    multiplayer: 'Lokal, Online',
+    rating: 5,
+    time: '6h 19min',
+  },
+  {
+    background: 'generic/ruby.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/1426210/header.jpg?t=1711529528',
+    description:
+      'In <b>It Takes Two</b> erlebt man zusammen mit einem Freund die Geschichte von zwei Eltern, die sich trennen wollen. Durch diese Reise sollen sie wieder zusammenfinden...',
+    story: true,
+    minplayers: 2,
+    published: '25.3.2021',
+    lastPlayed: '13.11.2021',
+    multiplayer: 'Online',
+    rating: 5,
+    time: '17h 9min',
+    achievements: {
+      progress: 0,
+      total: 20,
+    },
+  },
+  {
+    background: 'generic/gold.jpg',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/1410870/header.jpg?t=1709308099',
+    description:
+      '<b>Trikaya</b> ist ein Rätselspiel, das in einem alten Tempel in der Wüste spielt. Die Rätsel werden mit den Elementen Wind, Erde und Feuer gelöst',
+    story: true,
+    minplayers: 1,
+    published: '18.01.2021',
+    lastPlayed: '10.04.2021',
+    rating: 3,
+    time: '1h 10min',
+    achievements: {
+      progress: 4,
+      total: 4,
+    },
+  },
+  {
+    background: '',
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg?t=1710598764',
+    description:
+      'Mit <b>Wallpaper Engine</b> kann man Hintergrundbilder erstellen und herunterladen.',
+    minplayers: 0,
+    published: '11.2018',
+    lastPlayed: '23.3.2024',
+    rating: 5,
+    time: '103h 17min',
+    achievements: {
+      progress: 12,
+      total: 17,
+    },
+  },
+];
---
+++ index.js
@@ -0,0 +1,78 @@
+//10h Rubin
+//20h Smaragd
+//40h Diamant
+//80h Obsidian
+
+//33% Bronze
+//66% Silber
+//99% Gold
+document.addEventListener('DOMContentLoaded', () => {
+  for (card of data) {
+    elem = $(`
+        <div class="card" style="background-image: url(assets/${
+          card.background
+        })">
+          <img
+            src="${card.image}"
+          />
+          <div style="flex: 1"></div>
+          <p>${card.description}</p>
+          <div class="container">
+            <span class="story" style="visibility: ${
+              Object.hasOwn(card, 'story') ? 'visible' : 'hidden'
+            }">🕮 ${card.story ? '✓' : 'X'}</span>
+            <span class="players">${'𐀪'.repeat(
+              card.players - card.minplayers
+            )}${'<span style="color: blue">𐀪</span>'.repeat(
+      card.minplayers
+    )}</span>
+          </div>
+          <table>
+            <tr>
+              <th>Veröffentlichung</th>
+              <th>${card.published}</th>
+            </tr>
+            <tr${card.lastPlayed ? '' : ' style="display: none"'}>
+              <th>Zuletzt gespielt</th>
+              <th>${card.lastPlayed}</th>
+            </tr>
+            <tr${card.multiplayer ? '' : ' style="display: none"'}>
+              <th>Multiplayer Typ</th>
+              <th>${card.multiplayer}</th>
+            </tr>
+          </table>
+          <div style="flex: 1"></div>
+          <div style="height: 1.5em">
+            <span class="time" style="visibility: ${
+              card.time ? 'visible' : 'hidden'
+            }">${card.time}</span>
+            <span class="rating">${'<span style="color: yellow;display:inline-block;width:0.835em">★<span style="color: black; position: relative; left: -0.835em;">☆</span></span>'.repeat(
+              card.rating
+            )}${'☆'.repeat(5 - card.rating)}</span>
+          </div>
+          <div class="bar" style="display: ${
+            Object.hasOwn(card, 'achievements') ? 'block' : 'none'
+          }">
+            <div class="progress" style="clip-path: inset(0 ${
+              card.achievements
+                ? 100 -
+                  (card.achievements.progress * 100) / card.achievements.total
+                : 0
+            }% 0 0)"></div>
+            <div class="count">
+            ${card.achievements ? card.achievements.progress : 0} von ${
+      card.achievements ? card.achievements.total : 0
+    }<span style="float: right">${
+      card.achievements
+        ? Math.round(
+            (card.achievements.progress * 100) / card.achievements.total
+          )
+        : 0
+    }%</span>
+            </div>
+          </div>
+        </div>
+      `);
+    elem.appendTo($('body'));
+  }
+});
