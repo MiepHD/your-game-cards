fec86a3a08cba3dab7859838a12dcb7f3995d092
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-04-06 12:30:19 +0200

    Fixed book symbol?

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <div class="container">
             <span class="story"${
               Object.hasOwn(card, 'story') ? '' : ' style="display: none"'
-            }>&#x1F56E; ${
+            }>📖 ${
       typeof card.story === 'number'
         ? '&#x2713;'.repeat(card.story)
         : card.story
